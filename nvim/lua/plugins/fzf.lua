0f125ebe6ff8ab1466abdaab985389211b0a0923
lymanyang <lymanyang@tencent.com>
2025-05-19 14:10:51 +0800

    modify: nvim plugins

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -1,1 +1,7 @@
-return {}
+return {
+  "ibhagwan/fzf-lua",
+  keys = {
+    { "<leader>,", "<cmd>FzfLua files<cr>", desc = "find files" },
+    { "<leader><leader>", "<cmd>FzfLua buffers<cr>", desc = "find buffers" },
+  },
+}
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -50,116 +50,12 @@ F.configureKeybinds = function()
 end
 
 return {
-  {
-    "neovim/nvim-lspconfig",
-    init = function()
-      local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      -- disable keymaps
-      keys[#keys + 1] = { "K", false }
-      F.configureKeybinds()
-    end,
-
-    diagnostics = {
-      underline = true,
-      update_in_insert = false,
-      virtual_text = {
-        spacing = 4,
-        source = "if_many",
-        prefix = "●",
-        -- this will set set the prefix to a function that returns the diagnostics icon based on the severity
-        -- this only works on a recent 0.10.0 build. Will be set to "●" when not supported
-        -- prefix = "icons",
-      },
-      severity_sort = true,
-      signs = {
-        text = {
-          [vim.diagnostic.severity.ERROR] = LazyVim.config.icons.diagnostics.Error,
-          [vim.diagnostic.severity.WARN] = LazyVim.config.icons.diagnostics.Warn,
-          [vim.diagnostic.severity.HINT] = LazyVim.config.icons.diagnostics.Hint,
-          [vim.diagnostic.severity.INFO] = LazyVim.config.icons.diagnostics.Info,
-        },
-      },
-    },
-    -- Enable this to enable the builtin LSP inlay hints on Neovim >= 0.10.0
-    -- Be aware that you also will need to properly configure your LSP server to
-    -- provide the inlay hints.
-    inlay_hints = {
-      enabled = true,
-      exclude = {}, -- filetypes for which you don't want to enable inlay hints
-    },
-
-    opts = {
-      servers = {
-        -- Ensure mason installs the server
-        lua_ls = {},
-        bashls = {},
-        clangd = {
-          keys = {
-            { "<F4>", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
-          },
-          root_dir = function(fname)
-            return require("lspconfig.util").root_pattern(
-              "Makefile",
-              "configure.ac",
-              "configure.in",
-              "config.h.in",
-              "meson.build",
-              "meson_options.txt",
-              "build.ninja"
-            )(fname) or require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt")(
-              fname
-            ) or require("lspconfig.util").find_git_ancestor(fname)
-          end,
-          capabilities = {
-            offsetEncoding = { "utf-8" },
-          },
-          cmd = {
-            "clangd",
-            "--background-index",
-            "--clang-tidy",
-            "--header-insertion=iwyu",
-            "--completion-style=detailed",
-            "--function-arg-placeholders",
-            "--fallback-style=llvm",
-          },
-          init_options = {
-            usePlaceholders = true,
-            completeUnimported = true,
-            clangdFileStatus = true,
-          },
-        },
-
-        pyright = {},
-
-        neocmake = {
-          cmd = {
-            "neocmakelsp",
-            "--stdio",
-          },
-          filetypes = { "cmake" },
-          root_dir = function(fname)
-            require("lspconfig.util").find_git_ancestor(fname)
-          end,
-          single_file_support = true,
-          on_attach = function() end,
-          init_options = {
-            format = {
-              enable = true,
-            },
-            lint = {
-              enable = true,
-            },
-            scan_cmake_in_package = true,
-          },
-        },
-      },
-      setup = {
-        clangd = function(_, opts)
-          local clangd_ext_opts = LazyVim.opts("clangd_extensions.nvim")
-          require("clangd_extensions").setup(vim.tbl_deep_extend("force", clangd_ext_opts or {}, { server = opts }))
-          return false
-        end,
-      },
-    },
-  },
+  "neovim/nvim-lspconfig",
+  init = function()
+    F.configureKeybinds()
+  end,
+  opts = function()
+    local keys = require("lazyvim.plugins.lsp.keymaps").get()
+    keys[#keys + 1] = { "K", false }
+  end,
 }
