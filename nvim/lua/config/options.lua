84b3485ff0f7a382da1fef56d26fbfd1b4d7a04b
lymanyang <lymanyang@tencent.com>
2026-04-07 16:48:23 +0800

    osc52 remote不支持，改为本地缓存

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -8,15 +8,23 @@ vim.o.mouse = "a"
 vim.o.showmode = false
 vim.o.clipboard = "unnamedplus"
 if os.getenv("SSH_TTY") then
+  local osc52 = require("vim.ui.clipboard.osc52")
+  local cached = {}
   vim.g.clipboard = {
     name = "OSC 52",
     copy = {
-      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+      ["+"] = function(lines)
+        cached = lines
+        osc52.copy("+")(lines)
+      end,
+      ["*"] = function(lines)
+        cached = lines
+        osc52.copy("*")(lines)
+      end,
     },
     paste = {
-      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+      ["+"] = function() return cached end,
+      ["*"] = function() return cached end,
     },
   }
 end
